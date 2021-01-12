39344c90189285a8f6b9bcbf1a475327b53affc0
GeorgeYF <466912902@qq.com>
2021-01-12 19:25:15 +0800

    search组件引入，from标签

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="search-container">
-    搜索组件
+    <!-- 搜索栏 开始-->
+    <form action="/">
+      <van-search
+        v-model="searchText"
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="$router.back()"
+      />
+    </form>
+    <!-- 搜索栏 结束-->
+    <!-- 联系建议 开始-->
+    <!-- 联系建议 结束-->
+    <!-- 历史记录 开始-->
+    <!-- 历史记录 结束-->
+    <!-- 搜索结果 开始-->
+    <!-- 搜索结束 结束-->
   </div>
 </template>
 
@@ -10,7 +26,9 @@ export default {
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      searchText: ''
+    }
   },
   computed: {},
   watch: {},
@@ -18,7 +36,11 @@ export default {
   },
   mounted () {
   },
-  methods: {}
+  methods: {
+    onSearch () {
+      console.log('onSearch')
+    }
+  }
 }
 </script>
 
