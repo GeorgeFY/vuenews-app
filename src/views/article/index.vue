5e827c7740768c14b3d5918e9b5fa4684066b878
GeorgeYF <466912902@qq.com>
2021-01-13 18:31:57 +0800

    文章详情页面页面布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,6 +1,76 @@
 <template>
   <div class="article-container">
-    文章详情
+   <!-- 导航栏 -->
+    <van-nav-bar
+      class="app-nav-bar"
+      left-arrow
+      title="文章详情"
+      @click-left="$router.back()"
+    ></van-nav-bar>
+    <!-- /导航栏 -->
+    <div class="main-wrap">
+
+      <!-- 加载完成-文章详情 -->
+      <div class="article-detail">
+        <!-- 文章标题 -->
+        <h1 class="article-title">这是文章标题</h1>
+        <!-- /文章标题 -->
+
+        <!-- 用户信息 -->
+        <van-cell class="user-info" center :border="false">
+          <van-image
+            class="avatar"
+            slot="icon"
+            round
+            fit="cover"
+            src="https://img.yzcdn.cn/vant/cat.jpeg"
+          />
+          <div slot="title" class="user-name">作者名字</div>
+          <div slot="label" class="publish-date">14小时前</div>
+          <van-button
+            class="follow-btn"
+            type="info"
+            color="#3296fa"
+            round
+            size="small"
+            icon="plus"
+          >关注</van-button>
+        </van-cell>
+        <!-- /用户信息 -->
+
+        <!-- 文章内容 -->
+        <div class="markdown-body article-content">
+          这是文章内容
+        </div>
+        <van-divider>正文结束</van-divider>
+      </div>
+      <!-- /加载完成-文章详情 -->
+    </div>
+
+    <!-- 底部区域 -->
+    <div class="article-bottom">
+      <van-button
+        class="comment-btn"
+        type="default"
+        round
+        size="small"
+      >写评论</van-button>
+      <van-icon
+        name="comment-o"
+        info="123"
+        color="#777"
+      />
+      <van-icon
+        color="#777"
+        name="star-o"
+      />
+      <van-icon
+        color="#777"
+        name="good-job-o"
+      />
+      <van-icon name="share" color="#777777"></van-icon>
+    </div>
+    <!-- /底部区域 -->
   </div>
 </template>
 
@@ -8,6 +78,7 @@
 // 需要获取文章ID参数
 // 1 this.$router.params.xxx
 // 2 props传参  在路由中配置props:true 可以直接this.articleId使用
+import './github-markdown.css'
 export default {
   name: 'ArticleIndex',
   components: {},
@@ -28,5 +99,93 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.article-container {
+  .main-wrap {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 46px;
+    bottom: 44px;
+    overflow-y: scroll;
+    background-color: #fff;
+  }
+  .article-detail {
+    .article-title {
+      font-size: 20px;
+      padding: 14px;
+      margin: 0;
+      color: #3a3a3a;
+      background-color: #fff;
+    }
+
+    .user-info {
+      padding: 0 16px;
+      .avatar {
+        width: 35px;
+        height: 35px;
+        margin-right: 8px;
+      }
+      .van-cell__label {
+        margin-top: 0;
+      }
+      .user-name {
+        font-size: 12px;
+        color: #3a3a3a;
+      }
+      .publish-date {
+        font-size: 11px;
+        color: #b7b7b7;
+      }
+      .follow-btn {
+        width: 85px;
+        height: 29px;
+      }
+    }
+
+    .article-content {
+      padding: 14px;
+      /deep/ p {
+        text-align: justify;
+      }
+    }
+  }
+
+  .loading-wrap {
+    padding: 200px 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+  }
+
+  .article-bottom {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    box-sizing: border-box;
+    height: 44px;
+    border-top: 1px solid #d8d8d8;
+    background-color: #fff;
+    .comment-btn {
+      width: 141px;
+      height: 23px;
+      border: 1px solid #eeeeee;
+      font-size: 15px;
+      line-height: 23px;
+      color: #a7a7a7;
+    }
+    .van-icon {
+      font-size: 20px;
+      .van-info {
+        font-size: 11px;
+        background-color: #e22829;
+      }
+    }
+  }
+}
 </style>
