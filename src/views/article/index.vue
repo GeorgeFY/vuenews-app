1a231d8255b12951b23f0c740ae1ffa772570c35
GeorgeYF <466912902@qq.com>
2021-01-13 17:55:04 +0800

    文章详情页面搭建，路由配置,prop传参在文章详情页面获取文章ID参数，文章item点击to属性跳转

---
+++ src/views/article/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="article-container">
+    文章详情
+  </div>
+</template>
+
+<script>
+// 需要获取文章ID参数
+// 1 this.$router.params.xxx
+// 2 props传参  在路由中配置props:true 可以直接this.articleId使用
+export default {
+  name: 'ArticleIndex',
+  components: {},
+  props: {
+    articleId: {
+      type: [Number, String, Object],
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style>
+</style>
